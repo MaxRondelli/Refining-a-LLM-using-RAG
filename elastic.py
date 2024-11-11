fbda277b03f1f7898768ae3b01e00e929c47ad78
aleborrelli <ale.borrelli998@gmail.com>
2024-11-11 20:18:10 +0100

    added elastic search index creation and document ingestion

--- elastic.py
+++ elastic.py
@@ -1,7 +1,7 @@
-from elasticsearch import Elasticsearch 
-import os 
-from dotenv import load_dotenv 
-load_dotenv() 
+from elasticsearch import Elasticsearch
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 elastic_port_path = os.getenv("ELASTIC_HOST")
 elastic_username = os.getenv("ELASTIC_USERNAME")
@@ -14,22 +14,23 @@ client = Elasticsearch(
   ca_certs=ca_certs
 )
 
-client.info()
+index_name = "llm-index"
+index_settings = {
+    "number_of_shards": 1
+  }
+index_mapping = {
+  "properties": {
+    "source_file": {"type": "keyword"},
+    "page_number": {"type": "long"},
+    "plain_text": {"type": "text"},
+    "embeddings": {
+      "type": "dense_vector",
+      "dims": 3072
+    }
+  }
+}
 
-documents = [
-  { "index": { "_index": "index_name", "_id": "9780553351927"}},
-  {"name": "Snow Crash", "author": "Neal Stephenson", "release_date": "1992-06-01", "page_count": 470},
-  { "index": { "_index": "index_name", "_id": "9780441017225"}},
-  {"name": "Revelation Space", "author": "Alastair Reynolds", "release_date": "2000-03-15", "page_count": 585},
-  { "index": { "_index": "index_name", "_id": "9780451524935"}},
-  {"name": "1984", "author": "George Orwell", "release_date": "1985-06-01", "page_count": 328},
-  { "index": { "_index": "index_name", "_id": "9781451673319"}},
-  {"name": "Fahrenheit 451", "author": "Ray Bradbury", "release_date": "1953-10-15", "page_count": 227},
-  { "index": { "_index": "index_name", "_id": "9780060850524"}},
-  {"name": "Brave New World", "author": "Aldous Huxley", "release_date": "1932-06-01", "page_count": 268},
-  { "index": { "_index": "index_name", "_id": "9780385490818"}},
-  {"name": "The Handmaid's Tale", "author": "Margaret Atwood", "release_date": "1985-06-01", "page_count": 311},
-]
 
-client.bulk(operations=documents)
-client.search(index="index_name", q="snow")
+res = client.indices.create(index=index_name, mappings=index_mapping, settings=index_settings)
+
+print(res)
--- main.py
+++ main.py
@@ -1,44 +1,107 @@
+import json
 import os
-from dotenv import load_dotenv 
-load_dotenv() 
+import re
+
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from tqdm import tqdm
 
-FILE_PATH = ("dataset/BOOKS/CM_ALE_aooo_004_OCR.pdf")
+load_dotenv(".env")
+
+BOOKS_FOLDER = "dataset/BOOKS/"
+JSON_FOLDER = "dataset/JSON_FILES/"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TXT_FILE = 'dataset/output/CM_ALE_aooo_004_OCR.txt'
+elastic_port_path = os.getenv("ELASTIC_HOST")
+elastic_username = os.getenv("ELASTIC_USERNAME")
+elastic_pwd = os.getenv("ELASTIC_PWD")
+ca_certs = os.getenv("CA_CERTS_PATH")
+
+client = Elasticsearch(
+  elastic_port_path,
+  basic_auth=(elastic_username, elastic_pwd),
+  ca_certs=ca_certs
+)
+
+
+def clean_text(text):
+    # Remove unwanted special characters (such as strange symbols) but keep punctuation (.,;:)
+    text = re.sub(r'[^\w\s.,;:]', '', text)  # Keeps letters, numbers, spaces, and common punctuation
+    # Remove any formatting characters like tabs, newlines, etc.
+    text = re.sub(r'[•~^$%\*&@!\\/\t]', ' ', text)
+    # Replace multiple spaces with a single space
+    text = re.sub(r'\s+', ' ', text)
+    # Remove leading and trailing spaces
+    text = text.strip()
+
+    # Remove single newline characters without content (i.e., empty lines)
+    text = re.sub(r'(?<=\n)\n', '', text)
+
+    # Replace multiple newlines with a single newline
+    text = re.sub(r'\n+', '', text)
+
+    return text
+
 
 def len_func(text):
     return len(text)
 
-def main():               
-    loader = PyPDFLoader(FILE_PATH)
-    pages = []
-    for page in loader.lazy_load():
-        pages.append(page)
 
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-ada-002",
-    )
+def main() -> object:
+    for book_name in os.listdir(BOOKS_FOLDER):
+        book_json_folder = os.path.join(JSON_FOLDER, book_name.replace(".pdf", ""))
+        if not os.path.exists(book_json_folder):
+            os.mkdir(book_json_folder)
+            print(f" > Creating {book_json_folder} folder")
+            print(f" > Reading book: {book_name}")
+            loader = PyPDFLoader(os.path.join(BOOKS_FOLDER, book_name))
+            pages = []
+            metadata_l = []
+            for single_page in loader.lazy_load():
+                page = json.loads(single_page.json())
+                pages.append(clean_text(page["page_content"]))
+                metadata_l.append({
+                    "source_file": page["metadata"]["source"].split("/")[-1],
+                    "page_number": page["metadata"]["page"]
+                })
 
-    with open(TXT_FILE,'r') as f:
-        hp_book = f.read()
+            para_list = text_splitter.create_documents(texts=pages,
+                                                       metadatas=metadata_l)
 
-    text_splitter = CharacterTextSplitter(
-        separator="\n\n",
-        chunk_size = 1200,
-        chunk_overlap = 100,
-        length_function = len_func,
-        is_separator_regex= False)
+            for document in tqdm(para_list, " > Generating embeddings"):
+                doc = json.loads(document.json())
+                elastic_document = {
+                    "source_file": doc["metadata"]["source_file"],
+                    "page_number": doc["metadata"]["page_number"],
+                    "plain_text": doc["page_content"],
+                    "embeddings": embeddings.embed_query(doc["page_content"])
+                }
 
-    para_list = text_splitter.create_documents(texts = [hp_book])
-    docs = text_splitter.split_documents(para_list)
+                resp = client.index(index="llm-index", id=f"""{book_name}_{elastic_document['page_number']}""",
+                                    document=elastic_document)
+                with open(os.path.join(book_json_folder,
+                                       f"""page_{elastic_document['page_number']}_{resp["result"]}"""), 'w') as fp:
+                    json.dump(elastic_document, fp)
 
-    # Embeddings
-    single_vector = embeddings.embed_query(docs[1])
-    print(str(single_vector)[:100])
+
+        else:
+            print(f" > Book {book_name} already indexed")
+
+
+text_splitter = CharacterTextSplitter(
+    separator= ".",
+    chunk_size=1200,
+    chunk_overlap=100,
+    length_function=len_func,
+    is_separator_regex=False
+)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+)
 
 if __name__ == "__main__":
     main()
